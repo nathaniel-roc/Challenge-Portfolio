a43aec9025b91cc242e7dbcc9ffa631ad936d6e8
Nathaniel <nathaniel.dewaal@student.rocva.nl>
2020-11-23 10:31:48 +0100

    nav bar kleur veranderd

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,12 +1,16 @@
+//menu
 if (window.location.href == "http://localhost/challenge-portfolio/portfolio/" || window.location.href == "http://localhost/challenge-portfolio/portfolio/index.php" ){
     document.getElementById("about").style.backgroundColor = "#0ba7b3";
 }
+
 if (window.location.href == "http://localhost/challenge-portfolio/portfolio/interesses.php" ){
-    document.getElementById("about").style.backgroundColor = "#0ba7b3";
+    document.getElementById("interesses").style.backgroundColor = "#0ba7b3";
 }
+
 if (window.location.href == "http://localhost/challenge-portfolio/portfolio/projecten.php" ){
-    document.getElementById("about").style.backgroundColor = "#0ba7b3";
+    document.getElementById("projecten").style.backgroundColor = "#0ba7b3";
 }
+
 if (window.location.href == "http://localhost/challenge-portfolio/portfolio/contact.php" ){
-    document.getElementById("about").style.backgroundColor = "#0ba7b3";
+    document.getElementById("contact").style.backgroundColor = "#0ba7b3";
 }
---
+++ portfolio/projecten.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Projecten</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<?php require 'includes/menu.php'; ?>
+<br>
+<br>
+
+<?php require 'includes/footer.php'; ?>
+    <script src="js/script.js"></script>
+</body>
+</html>
